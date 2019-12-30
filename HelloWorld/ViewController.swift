8cbacf15a1073ddfb46821a1a4cd7d742ad59472
Ezequiel Coronel <ezequiel.coronel@etermax.com>
2019-12-30 18:19:56 -0300

    The project first commit with the new tutorial

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by ezequiel on 30/12/2019.
+//  Copyright © 2019 ezequiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
